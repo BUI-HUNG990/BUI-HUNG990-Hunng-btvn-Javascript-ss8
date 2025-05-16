8edeeb96e4ea5a442a65b264c0924fbc054f210b
BUI-HUNG990 <hung.buiduc050990@gmail.com>
2025-05-15 23:32:17 -0700

    Add files via upload

---
+++ baitapvenha.js
@@ -0,0 +1,65 @@
+// BÀI 1
+
+// let person = {
+//     name: "John Doe",
+//     age: 25,
+//     job: "Developer",
+// };
+// console.log("Tên:", person.name);
+// console.log("Tuổi:", person.age);
+// console.log("Công việc:", person.job);
+
+
+// BÀI 2
+
+// let person = {
+//     brand: "Toyota",
+//     model: "Corolla",
+//     year: "2020",
+// };
+// console.log(person);
+// // Thêm một thuộc tính mới là color với giá trị "red"
+// person.color = ["red"];
+// // Cập nhật giá trị của thuộc tính year thành 2022
+// person.year = "2022";
+// // In ra đối tượng sau khi cập nhật
+// for(let key in person){
+//     console.log(key, person[key]);
+// }
+
+
+// BÀI 3
+// let person = {
+//     title: "JavaScript",
+//     author: "John Smith",
+//     page: "200",
+// };
+// console.log(person);
+// // Xóa thuộc tính page khỏi đối tượng.
+// delete person.page;
+// console.log(person);
+// // In ra đối tượng sau khi đã xóa thuộc tính
+// for(let key in person){
+//     console.log(key, person[key]);
+// }
+
+
+// BÀI 4
+
+// const company = {
+//     name: "RikkeiSoft",
+//     location: "Hà Nội",
+//     employees: [
+//         { name: "Nguyễn Văn Luận", position: "Developer" },
+//         { name: "Nguyễn Văn Hoàng", position: "Tester" },
+//         { name: "Hoàng Nam Cao", position: "Manager" }
+//     ]
+// };
+// // Hiển thị tên công ty và tên của từng nhân viên
+// console.log("Tên công ty:", company.name);
+// company.employees.forEach(function(employee) {
+//     console.log(employee.name);
+// });
+
+
+// BÀI 7
